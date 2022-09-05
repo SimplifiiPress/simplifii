f7e76bbdec830700c4ac96a6faf08c57bcc25576
Leo Gopal <leo@leogopal.com>
2022-09-05 14:27:12 +0200

    phpcbf

--- functions.php
+++ functions.php
@@ -9,32 +9,30 @@
  * @subpackage Simplifii
  * @since Simplifii 1.0
  */
-if (!function_exists('simplifii_setup')) {
+if ( ! function_exists( 'simplifii_setup' ) ) {
 
 
 	/**
 	 * It adds support for the theme to use HTML5 markup, post thumbnails, and automatic feed links
 	 */
-	function simplifii_setup()
-	{
-		load_theme_textdomain('simplifii', get_template_directory() . '/languages');
+	function simplifii_setup() {
+		load_theme_textdomain( 'simplifii', get_template_directory() . '/languages' );
 
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support('automatic-feed-links');
+		add_theme_support( 'automatic-feed-links' );
 
 		// Add support for responsive embedded content.
-		add_theme_support('responsive-embeds');
+		add_theme_support( 'responsive-embeds' );
 
-		add_theme_support('woocommerce');
+		add_theme_support( 'woocommerce' );
 
 		// Add support for editor styles.
-		add_theme_support('editor-styles');
-		add_theme_support('block-nav-menus');
-
+		add_theme_support( 'editor-styles' );
+		add_theme_support( 'block-nav-menus' );
 
 		// Add support for post thumbnails.
-		add_theme_support('post-thumbnails');
-		set_post_thumbnail_size(1800, 9999);
+		add_theme_support( 'post-thumbnails' );
+		set_post_thumbnail_size( 1800, 9999 );
 
 		// HTML5 semantic markup.
 		add_theme_support(
@@ -46,15 +44,14 @@ if (!function_exists('simplifii_setup')) {
 		);
 	}
 }
-add_action('after_setup_theme', 'simplifii_setup');
+add_action( 'after_setup_theme', 'simplifii_setup' );
 
-if (!function_exists('simplifii_styles')) {
+if ( ! function_exists( 'simplifii_styles' ) ) {
 
 	/**
 	 * It registers two stylesheets, then enqueues a third stylesheet that depends on the first two
 	 */
-	function simplifii_styles()
-	{
+	function simplifii_styles() {
 		wp_register_style(
 			'simplifii-styles-blocks',
 			get_template_directory_uri() . '/assets/css/blocks.css'
@@ -77,20 +74,19 @@ if (!function_exists('simplifii_styles')) {
 			'simplifii-styles-front-end',
 			get_template_directory_uri() . '/assets/css/front-end.css',
 			false, //$dependencies,
-			wp_get_theme('simplifii')->get('Version')
+			wp_get_theme( 'simplifii' )->get( 'Version' )
 		);
 	}
 
 	// add_action('wp_enqueue_scripts', 'simplifii_styles');
 }
 
-if (!function_exists('simplifii_editor_styles')) {
+if ( ! function_exists( 'simplifii_editor_styles' ) ) {
 
 	/**
 	 * It registers two stylesheets, then enqueues a third stylesheet that depends on the first two
 	 */
-	function simplifii_editor_styles()
-	{
+	function simplifii_editor_styles() {
 		add_editor_style(
 			array(
 				'./assets/css/editor.css',
@@ -100,19 +96,19 @@ if (!function_exists('simplifii_editor_styles')) {
 		);
 	}
 
-	add_action('admin_init', 'simplifii_editor_styles');
+	add_action( 'admin_init', 'simplifii_editor_styles' );
 }
 
 
 /* It's checking if the function `register_block_pattern` exists. If it does, it will require the file
 `block-patterns.php` from the `inc/block-patterns` directory. */
-if (function_exists('register_block_pattern')) {
+if ( function_exists( 'register_block_pattern' ) ) {
 	require get_template_directory() . '/inc/block-patterns/block-patterns.php';
 }
 
 
 /* It's checking if the function `register_block_style` exists. If it does, it will require the file
 `block-styles.php` from the `inc/block-styles` directory. */
-if (function_exists('register_block_style')) {
+if ( function_exists( 'register_block_style' ) ) {
 	require get_template_directory() . '/inc/block-styles/block-styles.php';
 }
--- inc/block-styles/block-styles.php
+++ inc/block-styles/block-styles.php
@@ -9,21 +9,19 @@
  * @license GPL-2.0+
  */
 
-if (!function_exists('simplifii_register_block_styles')) {
+if ( ! function_exists( 'simplifii_register_block_styles' ) ) {
 	/**
 	 * Register Block styles
 	 */
-	function simplifii_register_block_styles()
-	{
-
-		if (!function_exists('register_block_style')) {
+	function simplifii_register_block_styles() {
+		if ( ! function_exists( 'register_block_style' ) ) {
 			return;
 		}
 
 		// No Gutters
 		/*
 		The above code is registering a new block style called "simplifii-no-gutters" with a label of "No Gutters".
-		
+
 		Args:
 		  : The name of the block style. This is the name that will be used to refer to the block style in the Block Style Control.
 		  : The label of the block style. This is what will be shown in the editor.
@@ -34,9 +32,9 @@ if (!function_exists('simplifii_register_block_styles')) {
 			'core/columns',
 			array(
 				'name'  => 'simplifii-no-gutters',
-				'label' => esc_html__('No Gutters', 'simplifii'),
+				'label' => esc_html__( 'No Gutters', 'simplifii' ),
 			)
 		);
 	}
-	add_action('init', 'simplifii_register_block_styles');
+	add_action( 'init', 'simplifii_register_block_styles' );
 }
